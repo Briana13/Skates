7a4ea8f134e8e2c3c4b0ab3ffed5d9ef8fba7883
LAPTOP-9JNP1D8O\herna <hernandezaguirrebriana@gmail.com>
2022-03-29 10:15:21 -0600

    messing around

--- starting/yee.java
+++ starting/yee.java
@@ -2,6 +2,22 @@ package starting;
 
 public class yee {
     public static void main(String[] args) {
-        System.out.println("Yeeee hewo!");
+        int myAge = 29;
+        int yourAge = 40;
+        int totalAge = 0;
+        //this block declares and initializes three variables
+     
+        System.out.println("myAge = " + myAge);
+        System.out.println("yourAge = " + yourAge);
+        System.out.println("totalAge = " + totalAge);
+        // this block prints the variable name, the equal sign, and the value stored in it
+     
+        totalAge = myAge + yourAge;
+        // puts the results from the right-hand side of the equal sign into the variable on the left of it
+     
+        System.out.println("totalAge = " + totalAge);
+        // prints new value stored in totalAge
+     
+        System.out.println("Yeeee hewo!\nhehe");
     }
 }
